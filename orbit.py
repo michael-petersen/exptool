0397c59b8c788461b2b11b4c9609192439568eca
Michael <petersen.michael.s@gmail.com>
2016-11-13 08:24:39 -0500

    revert orbit to definition structure

--- orbit.py
+++ orbit.py
@@ -20,88 +20,94 @@ import helpers
 '''
 #USAGE DEMO:
 
-O = orbit.TrackOrbits()
 
 tarr = np.arange(0,12,1,dtype='int')
-O.make_orbit_map('/path/to/outfile.dat','/path/to/exp/files',tarr,comp='dark')
+orbit.make_orbit_map('/path/to/outfile.dat','/path/to/exp/files',tarr,comp='dark')
 
-times,mass,orb = O.read_orbit_map('/path/to/outfile.dat')
+times,mass,orb = orbit.read_orbit_map('/path/to/outfile.dat')
 
 '''
 
+'''
 class TrackOrbits(object):
+
+    #
+    # do I really want this to be a class structure? this might make the definitions too clunky
+    #
 
     def __init__(self):
 
         pass 
-        #return None
+'''
 
-    def make_orbit_map(self,outfile,infile_template,time_array,norb=1,comp='star',verbose=0,**kwargs):
-        
-        #
-        # this writes to file because it is a lot to carry around
-        f = open(outfile,'wb')
-        #
-        
-        # check to see if an orbit list has been set
-        if 'orblist' in kwargs:
-            orbvals = kwargs['orblist'] # this needs to be passed as an integer array
-            norb = np.max(orbvals)+1
-            print 'N_orbits accepted: ',len(orbvals)
-        else:
-            orbvals = np.arange(0,norb,1,dtype='i')
+def make_orbit_map(outfile,infile_template,time_array,norb=1,comp='star',verbose=0,**kwargs):
 
-        
-        #
-        # print self-describing header to file
-        np.array([len(time_array),len(orbvals)],dtype=np.int).tofile(f)
-        #
+    #
+    # this writes to file because it is a lot to carry around
+    f = open(outfile,'wb')
+    #
 
-        # get time array from snapshots
-        times = []
-        for indx,val in enumerate(time_array):
-            O = psp_io.Input(infile_template+'%05i' %time_array[val],nout=1,comp=comp)
-            times.append(O.ctime)
-
-        # print to file
-        np.array(times,dtype=np.float).tofile(f)
-
-        # get mass array from snapshot
-        O = psp_io.Input(infile_template+'00000',nout=norb,comp=comp)
-        masses = O.mass[orbvals]
-
-        # print to file
-        np.array(masses,dtype=np.float).tofile(f)
-
-        # loop through files and extract orbits
-        for indx,val in enumerate(time_array):
-
-            O = psp_io.Input(infile_template+'%05i' %time_array[val],nout=norb,comp=comp,verbose=verbose)
-            
-            if verbose > 0: print O.ctime
-                
-            for star in orbvals:
-                np.array([O.xpos[star],O.ypos[star],O.zpos[star],O.xvel[star],O.yvel[star],O.zvel[star]],dtype=np.float).tofile(f)
-
-        f.close()
+    # check to see if an orbit list has been set
+    if 'orblist' in kwargs:
+        orbvals = kwargs['orblist'] # this needs to be passed as an integer array
+        norb = np.max(orbvals)+1
+        print 'N_orbits accepted: ',len(orbvals)
+    else:
+        orbvals = np.arange(0,norb,1,dtype='i')
 
 
-    def read_orbit_map(self,infile):
+    #
+    # print self-describing header to file
+    np.array([len(time_array),len(orbvals)],dtype=np.int).tofile(f)
+    #
 
-        # open file
-        f = open(infile,'rb')
+    # get time array from snapshots
+    times = []
+    for indx,val in enumerate(time_array):
+        O = psp_io.Input(infile_template+'%05i' %time_array[val],nout=1,comp=comp)
+        times.append(O.ctime)
 
-        # read header 
-        [ntimes,norb] = np.fromfile(f, dtype=np.int,count=2)
+    # print to file
+    np.array(times,dtype=np.float).tofile(f)
 
-        # read times and masses 
-        times = np.fromfile(f,dtype=np.float,count=ntimes)
-        mass = np.fromfile(f,dtype=np.float,count=norb)
-        
-        #print ntimes,norb
+    # get mass array from snapshot
+    O = psp_io.Input(infile_template+'00000',nout=norb,comp=comp)
+    masses = O.mass[orbvals]
 
-        orb = np.memmap(infile,offset=(16 + 8*ntimes + 8*norb),dtype=np.float,shape=(ntimes,norb,6))
-        
-        return times,mass,orb
+    # print to file
+    np.array(masses,dtype=np.float).tofile(f)
+
+    # loop through files and extract orbits
+    for indx,val in enumerate(time_array):
+
+        O = psp_io.Input(infile_template+'%05i' %time_array[val],nout=norb,comp=comp,verbose=verbose)
+
+        if verbose > 0: print O.ctime
+
+        for star in orbvals:
+            np.array([O.xpos[star],O.ypos[star],O.zpos[star],O.xvel[star],O.yvel[star],O.zvel[star]],dtype=np.float).tofile(f)
+
+    f.close()
+
+
+    
+
+def read_orbit_map(infile):
+
+    # open file
+    f = open(infile,'rb')
+
+    # read header 
+    [ntimes,norb] = np.fromfile(f, dtype=np.int,count=2)
+
+    # read times and masses 
+    times = np.fromfile(f,dtype=np.float,count=ntimes)
+    mass = np.fromfile(f,dtype=np.float,count=norb)
+
+    #print ntimes,norb
+
+    orb = np.memmap(infile,offset=(16 + 8*ntimes + 8*norb),dtype=np.float,shape=(ntimes,norb,6))
+
+    return times,mass,orb
 
 
