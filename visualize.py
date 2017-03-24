083f4363a7fde3dea359877d5ea94ab4aa6baf9f
Michael <petersen.michael.s@gmail.com>
2017-03-24 00:17:03 -0400

    improved velocity visualizations

--- visualize.py
+++ visualize.py
@@ -60,14 +60,21 @@ def show_dump(infile,comp,type='pos',transform=True,\
           levels,levels_edge = kde_pos(PSPDump,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
 
 
+    if ( (type=='Xvel') | (type=='Rvel') |  (type=='Tvel')):
 
-    if (type=='Xvel'):
+        if (type=='Xvel'): velarr = PSPDump.xvel
+        if (type=='Rvel'): velarr = (PSPDump.xpos*PSPDump.xvel + PSPDump.ypos*PSPDump.yvel)/(PSPDump.xpos*PSPDump.xpos + PSPDump.ypos*PSPDump.ypos)**0.5
+        if (type=='Tvel'): velarr = (PSPDump.xpos*PSPDump.yvel - PSPDump.ypos*PSPDump.xvel)/(PSPDump.xpos*PSPDump.xpos + PSPDump.ypos*PSPDump.ypos)**0.5
+
 
         kdeX,kdeY,XY,\
-          kdeZYz,kdeZYy,ZY,\
-          kdeXZx,kdeXZz,XZ,\
-          levels,levels_edge = kde_xvel(PSPDump,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
+              kdeZYz,kdeZYy,ZY,\
+              kdeXZx,kdeXZz,XZ,\
+              levels,levels_edge = kde_xvel(PSPDump,velarr,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
 
+
+        # reset to positive velocities for tangential case.
+        if (type=='Tvel'): levels = levels_edge = np.linspace(0.,np.max(levels),cres)
 
 
     fig = plt.figure(figsize=(7.,7.))
@@ -152,12 +159,15 @@ def kde_pos(PSPDump,gridsize=64,cres=24,face_extents=0.06,edge_extents=0.02,slic
       levels,levels_edge
 
 
-def kde_xvel(PSPDump,gridsize=64,cres=24,face_extents=0.06,edge_extents=0.02,slice_width=0.1):
+def kde_xvel(PSPDump,velarr,gridsize=64,cres=24,face_extents=0.06,edge_extents=0.02,slice_width=0.1,sncut=5.):
 
     # XY
-    kdeX,kdeY,kdeNUMXY = kde_3d.total_kde_two(PSPDump.xpos,PSPDump.ypos,gridsize=gridsize,extents=face_extents,weights=None,opt_third=abs(PSPDump.zpos),opt_third_constraint=slice_width)
+    kdeX,kdeY,kdeNUMXY = kde_3d.total_kde_two(PSPDump.xpos,PSPDump.ypos,\
+                                              gridsize=gridsize,extents=face_extents,weights=None,\
+                                              opt_third=abs(PSPDump.zpos),opt_third_constraint=slice_width)
     kdeNUMXY += 0.0001
-    kdeX,kdeY,kdeVELXY = kde_3d.total_kde_two(PSPDump.xpos,PSPDump.ypos,gridsize=gridsize,extents=face_extents,weights=PSPDump.xvel,opt_third=abs(PSPDump.zpos),opt_third_constraint=slice_width)
+    kdeNUMXY[np.where(kdeNUMXY < sncut)] = 1.e10
+    kdeX,kdeY,kdeVELXY = kde_3d.total_kde_two(PSPDump.xpos,PSPDump.ypos,gridsize=gridsize,extents=face_extents,weights=velarr,opt_third=abs(PSPDump.zpos),opt_third_constraint=slice_width)
 
 
     # XZ
@@ -165,23 +175,25 @@ def kde_xvel(PSPDump,gridsize=64,cres=24,face_extents=0.06,edge_extents=0.02,sli
                                                   gridsize=gridsize,extents=(-1.*face_extents,face_extents,-1.*edge_extents,edge_extents),\
                                                   weights=None,opt_third=abs(PSPDump.ypos),opt_third_constraint=slice_width)
     kdeNUMXZ += 0.0001
+    kdeNUMXZ[np.where(kdeNUMXZ < sncut)] = 1.e10
     kdeXZx,kdeXZz,kdeVELXZ = kde_3d.total_kde_two(PSPDump.xpos,PSPDump.zpos,\
                                                   gridsize=gridsize,extents=(-1.*face_extents,face_extents,-1.*edge_extents,edge_extents),\
-                                                  weights=PSPDump.xvel,opt_third=abs(PSPDump.ypos),opt_third_constraint=slice_width)
+                                                  weights=velarr,opt_third=abs(PSPDump.ypos),opt_third_constraint=slice_width)
 
     # ZY
     kdeZYz,kdeZYy,kdeNUMZY = kde_3d.total_kde_two(PSPDump.zpos,PSPDump.ypos,\
                                               gridsize=gridsize,extents=(-1.*edge_extents,edge_extents,-1.*face_extents,face_extents),\
                                               weights=None,opt_third=abs(PSPDump.xpos),opt_third_constraint=slice_width)
     kdeNUMZY += 0.0001
+    kdeNUMZY[np.where(kdeNUMZY < sncut)] = 1.e10
     kdeZYz,kdeZYy,kdeVELZY = kde_3d.total_kde_two(PSPDump.zpos,PSPDump.ypos,\
                                               gridsize=gridsize,extents=(-1.*edge_extents,edge_extents,-1.*face_extents,face_extents),\
-                                              weights=PSPDump.xvel,opt_third=abs(PSPDump.xpos),opt_third_constraint=slice_width)
+                                              weights=velarr,opt_third=abs(PSPDump.xpos),opt_third_constraint=slice_width)
 
-    maxlev_edge = np.max([np.max(abs(kdeVELXZ/kdeNUMXY)),np.max(abs(kdeVELZY/kdeNUMZY)),np.max(abs(kdeVELXZ/kdeNUMXZ))])
+    maxlev_edge = np.max([np.max(abs(kdeVELXY/kdeNUMXY)),np.max(abs(kdeVELZY/kdeNUMZY)),np.max(abs(kdeVELXZ/kdeNUMXZ))])
 
-    levels_edge = np.round(np.linspace(-1.*maxlev_edge,maxlev_edge,cres),1)
-    levels = np.round(np.linspace(-1.*maxlev_edge,maxlev_edge,cres),1)
+    levels_edge = np.round(np.linspace(-1.*maxlev_edge,maxlev_edge,cres),3)
+    levels = np.round(np.linspace(-1.*maxlev_edge,maxlev_edge,cres),3)
 
     print 'Increase factor:',np.max(levels)/np.max(levels_edge)
 
@@ -199,7 +211,7 @@ def kde_xvel(PSPDump,gridsize=64,cres=24,face_extents=0.06,edge_extents=0.02,sli
 def compare_dumps(infile1,infile2,comp,type='pos',transform=True,\
                   label1=None,label2=None,
               # parameters for the plot
-              gridsize=64,cres=24,face_extents=0.06,edge_extents=0.02,slice_width=0.1):
+              gridsize=64,cres=24,face_extents=0.06,edge_extents=0.025,slice_width=0.2):
     '''
     compare_dumps
         look at two different dumps for direct comparison
@@ -231,17 +243,29 @@ def compare_dumps(infile1,infile2,comp,type='pos',transform=True,\
           kdeXZx2,kdeXZz2,XZ2,\
           levels2,levels_edge2 = kde_pos(PSPDump2,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
 
-    if (type=='Xvel'):
+    if ( (type=='Xvel') | (type=='Rvel') |  (type=='Tvel')):
+        if (type=='Xvel'):
+            velarr1 = PSPDump1.xvel
+            velarr2 = PSPDump2.xvel
+        if (type=='Rvel'):
+            velarr1 = (PSPDump1.xpos*PSPDump1.xvel + PSPDump1.ypos*PSPDump1.yvel)/(PSPDump1.xpos*PSPDump1.xpos + PSPDump1.ypos*PSPDump1.ypos)**0.5
+            velarr2 = (PSPDump2.xpos*PSPDump2.xvel + PSPDump2.ypos*PSPDump2.yvel)/(PSPDump2.xpos*PSPDump2.xpos + PSPDump2.ypos*PSPDump2.ypos)**0.5
+        if (type=='Tvel'):
+            velarr1 = (PSPDump1.xpos*PSPDump1.yvel - PSPDump1.ypos*PSPDump1.xvel)/(PSPDump1.xpos*PSPDump1.xpos + PSPDump1.ypos*PSPDump1.ypos)**0.5
+            velarr2 = (PSPDump2.xpos*PSPDump2.yvel - PSPDump2.ypos*PSPDump2.xvel)/(PSPDump2.xpos*PSPDump2.xpos + PSPDump2.ypos*PSPDump2.ypos)**0.5
+
 
         kdeX1,kdeY1,XY1,\
-          kdeZYz1,kdeZYy1,ZY1,\
-          kdeXZx1,kdeXZz1,XZ1,\
-          levels1,levels_edge1 = kde_xvel(PSPDump1,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
+              kdeZYz1,kdeZYy1,ZY1,\
+              kdeXZx1,kdeXZz1,XZ1,\
+              levels1,levels_edge1 = kde_xvel(PSPDump1,velarr1,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
 
         kdeX2,kdeY2,XY2,\
-          kdeZYz2,kdeZYy2,ZY2,\
-          kdeXZx2,kdeXZz2,XZ2,\
-          levels2,levels_edge2 = kde_xvel(PSPDump2,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
+              kdeZYz2,kdeZYy2,ZY2,\
+              kdeXZx2,kdeXZz2,XZ2,\
+              levels2,levels_edge2 = kde_xvel(PSPDump2,velarr2,gridsize=gridsize,cres=cres,face_extents=face_extents,edge_extents=edge_extents,slice_width=slice_width)
+
+        if (type=='Tvel'): levels1 = levels_edge1 = levels2 = levels_edge2 = np.linspace(0.,np.max(levels),cres)
 
 
 
