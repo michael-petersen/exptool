e89ddc977e9fa83a6acbf56ba9a636d777c62000
Michael <petersen.michael.s@gmail.com>
2017-05-11 23:00:10 -0400

    make cmap==2 available

--- exptool/basis/accumulate_c/accumulate.c
+++ exptool/basis/accumulate_c/accumulate.c
@@ -1,17 +1,26 @@
 
 #include <stdio.h>
+#include <math.h>
 
 #include "accumulate.h"
 
 double r_to_xi(double r, int cmap, double scale)
 {
-  if (cmap) {
+  if ( cmap == 1 ) {
     
     if (r<0.0) {
       printf("radius < 0!");
       return 0.0;
 	} else {
       return (r/scale-1.0)/(r/scale+1.0);
+    }
+
+  } else if ( cmap == 2 ) {
+    if (r<=0.0) {
+      printf("radius <= 0!");
+      return 0.0;
+    } else {
+    return log(r);
     }
     
   } else {
@@ -30,11 +39,15 @@ double r_to_xi(double r, int cmap, double scale)
     
 double xi_to_r(double xi, int cmap, double scale)
 {
-  if (cmap) {
+  if ( cmap == 1 ) {
     if (xi<-1.0) printf("xi < -1!");
     if (xi>=1.0) printf("xi >= 1!");
 
-    say_hello();
+    // this is a dumb spot for this, but verify that C code can depend only upon itself inside the python wrapper (it can)
+    //say_hello();
+
+      } else if (cmap==2) {
+    return exp(xi);
 
     return (1.0+xi)/(1.0 - xi) * scale;
   } else {
@@ -46,11 +59,15 @@ double xi_to_r(double xi, int cmap, double scale)
 
 double d_xi_to_r(double xi, int cmap, double scale)
 {
-  if (cmap) {
+  if ( cmap == 1 ) {
     if (xi<-1.0) printf("xi < -1!");
     if (xi>=1.0) printf("xi >= 1!");
 
     return 0.5*(1.0-xi)*(1.0-xi)/scale;
+    
+      } else if (cmap==2) {
+    return exp(-xi);
+    
   } else {
     return 1.0;
   }
