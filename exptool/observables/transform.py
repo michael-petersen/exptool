b0a5ec662e00509a5de49e76005211e58b7497e0
Michael <petersen.michael.s@gmail.com>
2019-11-20 14:45:08 +0000

    add de_rotate_points

--- exptool/observables/transform.py
+++ exptool/observables/transform.py
@@ -78,3 +78,69 @@ def rotate_points(PSPDump,xrotation,yrotation,zrotation):
     
     return PSPOut
 
+
+
+def de_rotate_points(PSPDump,xrotation,yrotation,zrotation):
+    '''
+    rotate_points
+        take a PSP dump and return the positions/velocities derotated by a specified set of angles
+
+    simply the inverse of rotate_points.
+
+    inputs
+    ------------------
+    PSPDump
+    xrotation   : rotation into/out of page, in degrees
+    yrotation
+    zrotation
+
+
+
+    returns
+    ------------------
+    PSPOut      : the rotated phase-space output
+
+
+    '''
+    
+    radfac = np.pi/180.
+    
+    # set rotation in radians
+    a = xrotation*radfac#np.pi/2.2  # xrotation (the tip into/out of page)
+    b = yrotation*radfac#np.pi/3.   # yrotation
+    c = zrotation*radfac#np.pi      # zrotation
+    
+    # construct the rotation matrix
+    Rx = np.array([[1.,0.,0.],[0.,np.cos(a),np.sin(a)],[0.,-np.sin(a),np.cos(a)]])
+    Ry = np.array([[np.cos(b),0.,-np.sin(b)],[0.,1.,0.],[np.sin(b),0.,np.cos(b)]])
+    Rz = np.array([[np.cos(c),np.sin(c),0.,],[-np.sin(c),np.cos(c),0.],[0.,0.,1.]])
+    Rmatrix = np.linalg.inv(np.dot(Rx,np.dot(Ry,Rz)))
+    
+    # structure the points for rotation
+
+    # note: no guard against bad PSP here.
+    pts = np.array([PSPDump.xpos,PSPDump.ypos,PSPDump.zpos])
+    vpts = np.array([PSPDump.xvel,PSPDump.yvel,PSPDump.zvel])
+    
+    #
+    # instantiate new blank PSP item
+    PSPOut = psp_io.particle_holder()
+    
+    #
+    # do the transformation in position
+    tmp = np.dot(pts.T,Rmatrix)
+    PSPOut.xpos = tmp[:,0]
+    PSPOut.ypos = tmp[:,1]
+    PSPOut.zpos = tmp[:,2]
+    #
+
+    # and velocity
+    tmp = np.dot(vpts.T,Rmatrix)
+    PSPOut.xvel = tmp[:,0]
+    PSPOut.yvel = tmp[:,1]
+    PSPOut.zvel = tmp[:,2]
+    #
+    PSPOut.mass = PSPDump.mass
+    
+    return PSPOut
+
