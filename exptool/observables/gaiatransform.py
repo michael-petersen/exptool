d34d9303cd8994905b71b0ced22101fe8817f4a2
Michael <petersen.michael.s@gmail.com>
2020-09-25 20:10:42 +0100

    upgrade galactic transforms

--- exptool/observables/gaiatransform.py
+++ exptool/observables/gaiatransform.py
@@ -4,14 +4,16 @@ coordinates, as per Gaia instructions.
 
 # see https://gea.esac.esa.int/archive/documentation/GDR2/Data_processing/chap_cu3ast/sec_cu3ast_intro/ssec_cu3ast_intro_tansforms.html
 
+# TODO: find where I did the Cartesian velocities.
+# TODO: add the translation to the solar location (rather, away from)
 
 """
 
 
 import numpy as np
 
-
-
+"""
+# this is all legacy. compare and then delete if not needed
 def return_gaia_Agprime():
     """return the matrix in eq 3.61, key to transform from ICRS to galactic coordinates"""
     return np.array([[-0.0548755604162154,-0.8734370902348850,-0.4838350155487132],
@@ -145,5 +147,123 @@ def rotate_velocities_cartesian(a,d,r,mua,mud,vlos):
     
     return xdot,ydot,zdot
     
+"""
+
+
+def return_gaia_Agprime():
+    """return the matrix in eq 3.61, key to transform from ICRS to galactic coordinates"""
+    return np.array([[-0.0548755604162154,-0.8734370902348850,-0.4838350155487132],
+                     [+0.4941094278755837,-0.4448296299600112,+0.7469822444972189],
+                     [-0.8676661490190047,-0.1980763734312015,+0.4559837761750669]])
+
+def return_ricrs(a,d):
+    """ eq. 3.57"""
+    return np.array([np.cos(a)*np.cos(d),np.sin(a)*np.cos(d),np.sin(d)])
+
+def return_picrs(a,d):
+    """ eq. 3.64, unit vector of increasing alpha"""
+    if hasattr(a,'size'):
+        return np.array([-np.sin(a),np.cos(a),np.zeros(a.size)])
+    else:
+        return np.array([-np.sin(a),np.cos(a),0.])
+
+def return_qicrs(a,d):
+    """ eq. 3.64, unit vector of increasing delta"""
+    return np.array([-np.cos(a)*np.sin(d),-np.sin(a)*np.sin(d),np.cos(d)])
+
+def return_muicrs(a,d,mua,mud):
+    """ eq. 3.66, the proper motion vector"""
+    p = return_picrs(a,d)
+    q = return_qicrs(a,d)
+    return p*mua + q*mud
+
+
+
+def return_rgal(l,b):
+    """ eq. 3.58"""
+    return np.array([np.cos(l)*np.cos(b),np.sin(l)*np.cos(b),np.sin(b)])
+
+def return_pgal(l,b):
+    """ eq. 3.66, unit vector of increasing alpha"""
+    if hasattr(l,'size'):
+        return np.array([-np.sin(l),np.cos(l),0.*np.cos(l)])
+    else:
+        return np.array([-np.sin(l),np.cos(l),0.*np.cos(l)])
+
+def return_qgal(l,b):
+    """ eq. 3.66, unit vector of increasing delta"""
+    return np.array([-np.cos(l)*np.sin(b),-np.sin(l)*np.sin(b),np.cos(b)])
+
+def return_mugal(l,b,mul,mub):
+    """ eq. 3.66, the proper motion vector"""
+    p = return_pgal(l,b)
+    q = return_qgal(l,b)
+    return p*mul + q*mub
+
+
+def rotate_velocities(a,d,mua,mud):
+    """eq 3.68, """
+    mu = return_muicrs(a,d,mua,mud)
+    mugal = np.dot(return_gaia_Agprime(),mu) # eq. 3.68
     
+    # solve for positions
+    ricrs = return_ricrs(a,d)
+    rgal = np.dot(return_gaia_Agprime(),ricrs)
+
+    # implement eq 3.63
+    ell,b = np.arctan2(rgal[1],rgal[0]),np.arctan2(rgal[2],np.sqrt(rgal[0]*rgal[0]+rgal[1]*rgal[1]))
+    
+    p = return_pgal(ell,b)
+    q = return_qgal(ell,b)
+    
+    mul = np.sum(p*mugal,axis=0)
+    mub = np.sum(q*mugal,axis=0)
+    #print(mul,mub)
+    return mul,mub
+
+
+
+def rotate_errors(a,d,pmra_e,pmdec_e,pmcorr):
+    ricrs = return_ricrs(a,d)
+    picrs = return_picrs(a,d)
+    qicrs = return_qicrs(a,d)
+
+    rgal = np.dot(return_gaia_Agprime(),ricrs)
+
+    # implement eq 3.63
+    ell = np.arctan2(rgal[1],rgal[0])
+    b = np.arctan2(rgal[2],np.sqrt(rgal[0]*rgal[0]+rgal[1]*rgal[1]))
+
+    pgal = return_pgal(ell,b)
+    qgal = return_qgal(ell,b)
+
+    pqgal = np.stack((pgal, qgal), axis=-1)
+    pqicrs = np.stack((picrs, qicrs), axis=-1)
+
+    cov = np.array([[pmra_e*pmra_e,pmra_e*pmdec_e*pmcorr],[pmra_e*pmdec_e*pmcorr,pmdec_e*pmdec_e]])
+    #print(cov)
+
+    G = np.einsum('ab,ac->bc', pqgal,
+                      np.einsum('ji,ik->jk', return_gaia_Agprime(), pqicrs))
+
+    cov_to = np.einsum('ba,ac->bc', G,
+                           np.einsum('ij,ki->jk', cov, G))
+    
+    return cov_to
+
+#print(G)
+
+def rotate_positions(a,d,dist):
+    """eq 3.68, but built for speed"""
+    # solve for positions
+    if a.size>1:
+        ricrs = return_ricrs(a,d)
+        rgal = np.dot(return_gaia_Agprime(),ricrs)
+        cpos = dist*rgal        
+    else:
+        ricrs = return_ricrs(a,d)
+        rgal = np.dot(return_gaia_Agprime(),ricrs)
+        cpos = np.dot(dist,rgal)        
+    return cpos
+
 
