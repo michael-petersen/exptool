53838fa8d08667a62eaa4d07164a8c51d1005821
Michael <petersen.michael.s@gmail.com>
2022-01-21 18:18:05 +0100

    Where did read_sph_model_table go?

--- exptool/utils/halo_methods.py
+++ exptool/utils/halo_methods.py
@@ -53,6 +53,14 @@ xi,rarr,p0,d0 = halo_methods.init_table(model_file,numr,rmin,rmax,cmap=cmap,scal
 
 '''
 
+def read_sph_model_table(modelfile):
+    """needs to be made more flexible, but this will at least not break!"""
+
+    A = np.genfromtxt(modelfile,comments='!',skip_header=5)
+    print(A[0])
+    R1,D1,M1,P1 = A[:,0],A[:,1],A[:,2],A[:,3]
+    return R1,D1,M1,P1
+
 
 def parse_slgrid(file,verbose=0):
     f = open(file,'rb')
@@ -68,7 +76,7 @@ def parse_slgrid(file,verbose=0):
     rmin = b[0]
     rmax = b[1]
     scale = b[2]
-    
+
     if verbose > 0:
         print('LMAX=',lmax)
         print('NMAX=',nmax)
@@ -79,7 +87,7 @@ def parse_slgrid(file,verbose=0):
         print('SCALE=',scale)
 
     f.close()
-    
+
     return lmax,nmax,numr,cmap,rmin,rmax,scale
 
 
@@ -138,14 +146,14 @@ def read_cached_table(file,verbose=0,retall=True):
     eftable = np.ones([lmax+1,nmax,numr])
     #
     #
-    
-    for l in range(0,lmax+1): 
+
+    for l in range(0,lmax+1):
         #
         # The l integer
         #
         ltable[l] = np.fromfile(f, dtype=np.uint32,count=1)
         evtable[l,0:nmax] = np.fromfile(f,dtype='f8',count=nmax)
-        
+
         for n in range(0,nmax):
             if verbose==1: print(l,n)
             #
@@ -154,7 +162,7 @@ def read_cached_table(file,verbose=0,retall=True):
             eftable[l,n,:] = np.fromfile(f,dtype='f8',count=numr)
 
     f.close()
-    
+
     if retall:
         return lmax,nmax,numr,cmap,rmin,rmax,scale,ltable,evtable,eftable
     else:
@@ -163,7 +171,7 @@ def read_cached_table(file,verbose=0,retall=True):
 
 
 
-    
+
 
 
 
@@ -172,27 +180,27 @@ def init_table(modelfile,numr,rmin,rmax,cmap=0,scale=1.0,spline=True):
     make the model table and the cachefile agree on abscissa
     """
     R1,D1,M1,P1 = read_sph_model_table(modelfile)
-    
+
     fac0 = 4.*np.pi
     xi = np.zeros(numr)
     r = np.zeros(numr)
     p0 = np.zeros(numr)
     d0 = np.zeros(numr)
-    
+
     if (cmap==1):
         xmin = (rmin/scale - 1.0)/(rmin/scale + 1.0);
         xmax = (rmax/scale - 1.0)/(rmax/scale + 1.0);
-                 
+
     if (cmap==2):
         xmin = log(rmin);
         xmax = log(rmax);
-        
+
     if (cmap==0):
         xmin = rmin;
         xmax = rmax;
-        
+
     dxi = (xmax-xmin)/(numr-1);
-        
+
     #
     #
     if spline==True:
@@ -210,5 +218,3 @@ def init_table(modelfile,numr,rmin,rmax,cmap=0,scale=1.0,spline=True):
             p0[i] = interpolate.splev(r[i], pfunc, der=0)
             d0[i] = interpolate.splev(r[i], dfunc, der=0)
     return xi,r,p0,d0
-
-
